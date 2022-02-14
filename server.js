80db5e5573dfe6f42ad519a0e1ca779b32745306
ayushkul <akcool.ayush.svm@gmail.com>
2022-02-14 20:15:05 +0530

    AMQP, BlockChain Follower, TxReceipt

--- server.js
+++ server.js
@@ -9,42 +9,23 @@ const app = new APP();
 require("./config/express")(app);
 import BlockController from "./app/modules/block";
 import WebSocketService from "./app/service/WebsocketService";
+import AMQP from "./library";
 
-const IOSocket = require('socket.io');
 global.lhtWebLog = Utils.lhtLog;
-process.on('unhandledRejection', error => {
-    // Will print "unhandledRejection err is not defined"
-    console.log('unhandledRejection', error.message);
-});
 
 class Server {
     static listen() {
-        Promise.all([DBConnection.connect()])
+        Promise.all([DBConnection.connect(), AMQP.conn(Config.AMQP_HOST_URL, true)])
             .then(async () => {
-                let server = app.listen(Config.PORT);
-                let socketIO = IOSocket(server, {
-                    cors: {
-                        origin: Config.ORIGIN_URL,
-                        methods: ["GET", "POST"],
-                        allowedHeaders: ["*"],
-                        credentials: true
-                    }
-                });
-                Utils.lhtLog("listen", `Server Started on port ${Config.PORT}`, {}, "Developer", httpConstants.LOG_LEVEL_TYPE.INFO);
                 routes(app);
+                app.listen(Config.PORT);
                 global.web3 = await WebSocketService.webSocketConnection(Config.WS_URL);
-                require("./config/jobInitializer");
-                socketIO.on('connection', (socket) => {
-                    socket.on('Connected', () => {
-                        Utils.lhtLog("listen", `socket connected`, {}, "Developer", httpConstants.LOG_LEVEL_TYPE.INFO);
-                    })
-                });
-                
-                BlockController.listenBlocks(socketIO);
+                await BlockController.listenBlocks(web3);
+                Utils.lhtLog("listen", `Server Started on port ${Config.PORT}`);
             })
             .catch((error) => {
                 global.web3 = WebSocketService.webSocketConnection(Config.WS_URL);
-                Utils.lhtLog("listen", "failed to connect", {err: error}, "Developer", httpConstants.LOG_LEVEL_TYPE.ERROR)
+                Utils.lhtLog("listen", "Failed to connect", {err: error}, "Developer", httpConstants.LOG_LEVEL_TYPE.ERROR)
             });
     }
 }
