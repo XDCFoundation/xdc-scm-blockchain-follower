2fa3e6e28a84a249fa7637ea99c67210b420ae9a
kajal_KajalBobal <kajal@leewayhertz.com>
2022-03-11 15:33:53 +0530

    websocket connection function updated

--- app/service/WebsocketService.js
+++ app/service/WebsocketService.js
@@ -4,7 +4,20 @@ export default class WebSocketService {
     static webSocketConnection(url) {
         try {
             console.log(url)
-            return new Web3((url));
+            return new Web3(new Web3.providers.WebsocketProvider(url,
+                {
+                    clientConfig: {
+                        keepalive: true,
+                        keepaliveInterval: 60000,
+                    },
+                    reconnect: {
+                        auto: true,
+                        delay: 2500,
+                        onTimeout: true,
+                    }
+                }
+                )
+           )
         } catch (err) {
             console.log("webSocketConnection err", err);
             global.web3 = new Web3((url));
